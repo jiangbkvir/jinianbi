82a4ce5e22556fbd8abdd7a77f9995046c7ce2d8
jiangbkvir <80749242+jiangbkvir@users.noreply.github.com>
2026-01-15 16:10:55 +0800

    Test: Modify script without updating version

--- 纪念币预约辅助工具.user.js
+++ 纪念币预约辅助工具.user.js
@@ -24,6 +24,7 @@
 // @license      MIT
 // @downloadURL https://update.greasyfork.org/scripts/521357/%E7%BA%AA%E5%BF%B5%E5%B8%81%E9%A2%84%E7%BA%A6%E8%BE%85%E5%8A%A9%E5%B7%A5%E5%85%B7.user.js
 // @updateURL  https://update.greasyfork.org/scripts/521357/%E7%BA%AA%E5%BF%B5%E5%B8%81%E9%A2%84%E7%BA%A6%E8%BE%85%E5%8A%A9%E5%B7%A5%E5%85%B7.user.js
+// @test-comment Gemini-CLI-test-no-version-change
 // ==/UserScript==
 
 
